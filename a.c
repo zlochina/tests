3db8b6d91de55ba0d99285c50fe04add497d873e
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2023-12-30 15:59:11 +0100

    Added new feature in a.c

--- a.c
+++ a.c
@@ -1,6 +1,19 @@
 #include "a.h"
 
+#include <stdio.h>
 #include <stdlib.h>
+
+int new_feature(int num) {
+  char *buffer = NULL;
+  buffer = (char *)malloc(num + 1);
+  if (buffer == NULL) exit(EXIT_FAILURE);
+  for (int i = 0; i < num; i++) {
+    buffer[i] = '-';
+  }
+  buffer[num] = '\0';
+  printf("Seperator: %s\n", buffer);
+  return EXIT_SUCCESS;
+}
 
 int main(void) {
   // SOMETHING
