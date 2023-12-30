ab8d63744d13a85214d4183be1695050b85faf20
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2023-12-30 14:07:51 +0100

    Added new feature implementation in a.c

--- a.c
+++ a.c
@@ -1,6 +1,20 @@
 #include "a.h"
 
+#include <stdio.h>
 #include <stdlib.h>
+
+int new_feature_print(int num) {
+  char *buff = NULL;
+  buff = (char *)malloc(num + 1);
+  if (buff == NULL) return EXIT_FAILURE;
+
+  for (int i = 0; i < num; i++) {
+    buff[i] = '-';
+  }
+  buff[num] = '\0';
+  printf("%s\n", buff);
+  return EXIT_SUCCESS;
+}
 
 int main(void) {
   // SOMETHING
