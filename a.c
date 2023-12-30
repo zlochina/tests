4d1cb6e9e6b59b561834e86fe53e3c310e9783dc
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2023-12-30 14:13:24 +0100

    Some changes in main branch

--- a.c
+++ a.c
@@ -1,6 +1,12 @@
 #include "a.h"
 
+#include <stdio.h>
 #include <stdlib.h>
+
+int some_main_function(int numbers) {
+  fprintf(stderr, "[INFO]: %d.", numbers);
+  return EXIT_SUCCESS;
+}
 
 int main(void) {
   // SOMETHING
